f4510b0878eb9ba1ff2de75c7ad1232cef61c7fe
Alex424M <alex424moroz@gmail.com>
2022-05-01 21:17:25 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('.form__input');
 function validate_form() {
     let valid = true;
     for (let i = 0; i < inputs.length; i++) {
-        inputs[i].style.border = "0px solid red";
+        inputs[i].style.border = "none";
         if (inputs[i].value == "") {
             valid = false;
             inputs[i].style.border = "1px solid red";
